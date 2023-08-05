b0735c47b537d15310bdd66cafa8cae25e2cdb1b
Juffin001 <magdeeviliaz24@yandex.ru>
2023-08-05 05:59:03 +0400

    v 1.0.0

---
+++ attention_site/main_app/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('login/', views.LoginUser.as_view(), name = 'login'),
+    path('register/', views.RegisterUser.as_view(), name = 'register'),
+    path('logout/', views.logout_user, name = 'logout'),
+    path('main/', views.main_screen_yo, name='main_screen')
+]
---
+++ attention_site/main_app/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout, login
+from django.views.generic import CreateView
+from .forms import *
+'''
+bootstrap быстрый сделать
+'''
+def index(request):
+    return render(request, "main_app/main_page.html", {})
+
+
+class RegisterUser(CreateView):
+    form_class = UserCreationForm
+    template_name = 'main_app/register.html'
+    success_url = reverse_lazy('login')
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        #c_def = self.get_user_context(title="Register")
+        return dict(list(context.items()))# + list(c_def.items())
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('index')
+    
+
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = "main_app/login.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        #c_def = self.get_user_context(title="Enter acc")
+        return dict(list(context.items()))# + list(c_def.items())
+    def get_success_url(self):
+        return reverse_lazy('main_screen')
+
+def logout_user(request):
+    logout(request)
+    return redirect('index')
+
+def main_screen_yo(request):
+    return render(request, "main_app/main_screen.html", {})
